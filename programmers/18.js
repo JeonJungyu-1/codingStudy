127c75b6ca54ba966499f559042231b2088a75e2
JeonJungyu-1 <wjswnsrb93@naver.com>
2022-09-25 11:52:46 +0900

    study

--- programmers/18.js
+++ programmers/18.js
@@ -2,34 +2,62 @@
 function solution(rows, columns, queries) {
     let answer = [];
     let table = [];
-    for (let i = 0; i < columns; i++) {
+    for (let i = 0; i < rows; i++) {
         table.push([]);
-        for (let j = 0; j < rows; j++) {
+        for (let j = 0; j < columns; j++) {
             table[i].push(i * columns + j + 1);
         } 
-    }
-    
-    // console.log(table);
+    }        
+    const tmpArr = table.map((el) => [...el]);
 
-    queries.map((el, index) => {
-        const m = el[2] - el[0];
-        const n = el[3] - el[1];
-        console.log(m, n);
-        const tmpArr = table.map((el) => [...el]);
-
-        for (let i = 0; i < m; i++ ) {
-            for (let j = 0; j < n; j++) {
-                tmpArr[i][j] = table[m-1-j][i];
+    for (let el of queries) {
+        let minNum = 0;
+        
+        function isMinNum(num) {
+            if (!minNum || minNum > num) {
+                minNum = num;
             }
         }
         
-        for (let i = 0; i < m; i++ ) {
-            for (let j = 0; j < n; j++) {
-                tmpArr[i][j] = table[m-1-j][i];
-            }
-        }  
-    })
-    // if (rows)
-    // console.log(tmpArr);
+        if (el[0] === 1 && el[1] === 1 && el[2] === columns && el[3] === rows) {
+            answer.push(1);
+            continue;
+        } 
+        
+        //임시배열에 차례대로 회전한 값 담기
+        for (let i = el[1] - 1; i < el[3] - 1; i++) {
+            tmpArr[el[0] - 1][i + 1] = table[el[0] - 1][i];
+        }
+        for (let i = el[0] - 1; i < el[2] - 1; i++) {
+            tmpArr[i + 1][el[3] - 1] = table[i][el[3] - 1];
+        }
+        for (let i = el[1] - 1; i < el[3] - 1; i++) {
+            tmpArr[el[2] - 1][i] = table[el[2] - 1][i + 1];
+        }
+        for (let i = el[0] - 1; i < el[2] - 1; i++) {
+            tmpArr[i][el[1] - 1] = table[i + 1][el[1] - 1];
+        }
+        
+        
+        //임시배열에서 값 다시 가져오기
+        for (let i = el[1] - 1; i < el[3] - 1; i++) {
+            table[el[0] - 1][i + 1] = tmpArr[el[0] - 1][i + 1];
+            isMinNum(table[el[0] - 1][i + 1]);
+        }
+        for (let i = el[0] - 1; i < el[2] - 1; i++) {
+            table[i + 1][el[3] - 1] = tmpArr[i + 1][el[3] - 1];
+            isMinNum(table[i + 1][el[3] - 1]);
+        }
+        for (let i = el[1] - 1; i < el[3] - 1; i++) {
+            table[el[2] - 1][i] = tmpArr[el[2] - 1][i];
+            isMinNum(table[el[2] - 1][i]);
+        }
+        for (let i = el[0] - 1; i < el[2] - 1; i++) {
+            table[i][el[1] - 1] = tmpArr[i][el[1] - 1];
+            isMinNum(table[i][el[1] - 1]);
+        }
+        
+        answer.push(minNum);
+    }
     return answer;
 }
