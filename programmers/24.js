d7d26ef404fc749b21359902094b6bf686165551
JeonJungyu-1 <wjswnsrb93@naver.com>
2022-10-02 17:05:57 +0900

    study

--- programmers/24.js
+++ programmers/24.js
@@ -1,68 +1,66 @@
 //거리두기 확인하기
 function solution(places) {
     let answer = [];
-    let distance;
-    places.map((el) => {
+    
+    places.map((el, index) => {
         for (let j = 0; j < 5; j++) {
-            // if (el[j].indexOf("P") === el[j].lastIndexOf("P")) {
-                // answer.push(columnCheck(places, j, i));
-                // break;
-            // } else {
-                for (let i = 0; i < 5; i++) {
-                    distance = el[j].indexOf("P", i + 1) - el[j].indexOf("P", i);
-                    if (distance === 1) {
-                        answer.push(0);
+            for (let i = 0; i < 5; i++) {
+                if (el[j][i] === "P") {
+                    if (i+1<=4 && el[j][i+1] === "P") {
+                        answer[index] = 0;
                         break;
-                    } else if (distance === 2) {
-                        if (el[j][el[j].indexOf("P", i + 1) + 1] === "X") {
-                            answer.push(0);
+                    } else if (i+2<=4 && el[j][i+2] === "P") {
+                        if (i+1 <=4 && el[j][i + 1] !== "X") {
+                            answer[index] = 0;
                             break;
                         }
-                    } else {
-                        answer.push(columnCheck(places, j, i));
+                    } 
+
+                    answer[index] =columnCheck(el, j, i, index);
+                    if (answer[index] === 0) {
                         break;
+                    } else {
+                        continue;
                     }
                 }
-                if (!answer[j]) {
-                    break;
-                }
-            // }
+            }
+            if (answer[index] === 0) {
+                break;
+            }
         }
     })
     
-    function rowCheck() {
-        
-    }
     
-    function columnCheck(places, j, i) {
-        if ((j+1 <= 4 && places[j+1][i] === "P") || (j-1 >= 0 && places[j-1][i] === "P") ) {
+    function columnCheck(places, j, i, index) {
+        if (j-1 >= 0 && places[j-1][i] === "P") {
             return 0;
-        } else if (j+1<=4 && places[j + 1][i + 1] === "P") {
+        }
+        if (j+1<=4 && places[j + 1][i + 1] === "P") {
             if (places[j + 1][i] !== "X" || places[j][i + 1] !== "X") {
                 return 0;
             }
-        } else if (j+1<=4 && places[j + 1][i - 1] === "P") {
+        }
+        if (j+1<=4 && places[j + 1][i - 1] === "P") {
             if (places[j + 1][i] !== "X" || places[j][i - 1] !== "X") {
                 return 0;
             }
-        } else if (j-1>=0 && places[j - 1][i + 1] === "P") {
-            if (places[j - 1][i] !== "X" || places[j][i + 1] !== "X") {
-                return 0;
-            }
-        } else if (j-1 >=0 && places[j - 1][i - 1] === "P") {
-            if (places[j - 1][i] !== "X" || places[j][i - 1] !== "X") {
-                return 0;
-            }
-        } else if (j-2>=0 && places[j - 2][i] === "P") {
+        }
+
+        if (j-2>=0 && places[j - 2][i] === "P") {
             if (places[j - 1][i] !== "X") {
                 return 0;
             }
-        } else if (j+2<=4 && places[j + 2][i] === "P") {
-            if (places[j + 1][i] !== "X") {
-                return 0;
-            }
+        }
+       
+        return 1;
+    }
+    
+    for (let i = 0; i < 5; i++) {
+        if (answer[i] === undefined) {
+            answer[i] = 1;
         }
     }
+    
     
     return answer;
 }
