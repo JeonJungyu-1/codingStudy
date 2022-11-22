2418cb4be89db648578c66bc9267990e983d049b
JeonJungyu-1 <wjswnsrb93@naver.com>
2022-11-22 17:58:11 +0900

    예산

---
+++ programmers/68.js
@@ -0,0 +1,14 @@
+//예산
+function solution(d, budget) {
+    let answer = 0;
+    d.sort((a, b) => a - b);
+    for (let i = 0; i < d.length; i++) {
+        if (budget < d[i]) {
+            break;
+        } else {
+            budget -= d[i];
+            answer++;
+        }
+    }
+    return answer;
+}
