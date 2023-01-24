ba9b13e146bbaa52246a769981645bdad5983f2f
JeonJungyu <wjswnsrb93@naver.com>
2023-01-24 22:23:40 +0900

    baekjoon 1181

---
+++ baekjoon/BJ1181.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BJ1181 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<String> strSet = new HashSet<>();
+        int num = Integer.parseInt(br.readLine());
+        for (int i = 0; i < num; i++) {
+            strSet.add(br.readLine());
+        }
+
+        List<String> strList = new ArrayList<>(strSet);
+
+        strList.sort(new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() > o2.length()) {
+                    return 1;
+                } else if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return -1;
+                }
+            }
+            
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strList.size(); i++) {
+            sb.append(strList.get(i)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
