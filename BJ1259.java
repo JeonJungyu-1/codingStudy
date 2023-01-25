6ff463fff663b93b505a9d6d3499e4f9c57d9142
JeonJungyu <wjswnsrb93@naver.com>
2023-01-25 21:51:04 +0900

    baekjoon 1259

---
+++ BJ1259.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ1259 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder outputStr = new StringBuilder("");
+        while (true) {
+            String str = br.readLine();
+            StringBuilder sb = new StringBuilder(str);
+            if (str.equals("0")) {
+                break;
+            }
+
+            
+
+            if (str.equals(sb.reverse().toString())) {
+                outputStr.append("yes").append("\n");
+            } else {
+                outputStr.append("no").append("\n");
+            }
+        }
+
+        System.out.println(outputStr);
+    }
+}
