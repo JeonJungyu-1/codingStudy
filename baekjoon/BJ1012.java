360764e735b30cde62a736ff50f8f0b467c322d2
JeonJungyu <wjswnsrb93@naver.com>
2023-01-27 22:27:51 +0900

    baekjoon class 3

---
+++ baekjoon/BJ1012.java
@@ -0,0 +1,75 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ1012 {
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static int[][] map;
+    static int min;
+    
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int num = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        // while (st.hasMoreTokens()) {
+        //     st.nextToken();
+        // }
+
+        for (int i = 0; i < num; i++) {
+            min = 0;
+            st = new StringTokenizer(br.readLine());
+            int column = Integer.parseInt(st.nextToken());
+            int row = Integer.parseInt(st.nextToken());
+            map = new int[row][column];
+            
+            for (int m = 0; m < map.length; m++) {
+                Arrays.fill(map[m], 0);
+            }
+
+            int count = Integer.parseInt(st.nextToken());
+            // 초기 배추 설정
+            for (int j = 0; j < count; j++) {
+                st = new StringTokenizer(br.readLine());
+                int c = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                map[r][c] = 1;
+            }
+
+            // 배추 탐색
+            for (int k = 0; k < map.length; k++) {
+                for (int l = 0; l < map[k].length; l++) {
+                    if (map[k][l] == 1) {
+                        checkAround(k, l);
+
+                        
+                         min++;
+                    }
+                }
+            }
+            sb.append(min).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    //탐색하고 -1로 바꾸면 상관 없을듯
+    static void checkAround(int r, int c) {
+        map[r][c] = -1;
+        for (int i = 0; i < 4; i++) {
+            if (!checkIndex(r + dx[i], c + dy[i]) && map[r + dx[i]][c + dy[i]] == 1) {
+                checkAround(r + dx[i], c + dy[i]);
+            }
+        }
+    }
+
+    static boolean checkIndex(int r, int c) {
+        return r < 0 || r >= map.length || c < 0 || c >= map[0].length;
+    }
+}
---
+++ baekjoon/BJ7576.java
@@ -0,0 +1,180 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//시간 초과 코드 
+// public class BJ7576 {
+//     static int[][] map;
+//     static int day = 0;
+//     static int[] x = {0, 0, 1, -1};
+//     static int[] y = {-1, 1, 0, 0};
+//     static int count;
+//     static Queue<Integer> queue = new LinkedList<>();
+
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         String[] mapSize = br.readLine().split(" ");
+
+
+//         //맵 생성
+//         map = new int[Integer.parseInt(mapSize[1])][Integer.parseInt(mapSize[0])];
+//         for (int i = 0; i < map.length; i++) {
+//             map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+//         }
+
+//         while (true) {
+//             count = 0;
+//             for (int i = 0; i < map.length; i++) {
+//                 for (int j = 0; j < map[i].length; j++) {
+//                     if (map[i][j] == day + 1) {
+//                         checkAround(i, j);     
+//                         map[i][j]++;                   
+//                     }
+//                 }
+//             }
+
+//             if (count == 0) {
+//                 if (!checkZero()) {
+//                     day = -1;
+//                 }
+//                 break;
+//             }
+//             day++;
+//         }
+
+//         System.out.println(day);
+
+//     }
+
+    
+
+//     static void checkAround(int r, int c) {
+//         for (int i = 0; i < 4; i++) {
+//             if (indexCheck(r + x[i], c + y[i]) && (map[r + x[i]][c + y[i]] == 0)) {
+//                     map[r + x[i]][c + y[i]] = day + 2;
+//                     count++;
+                
+//             }
+//         }
+//     }
+
+//     static boolean checkZero() {
+//         int zeroCheck = 0;
+//         for (int i = 0; i < map.length; i++) {
+//             for (int j = 0; j < map[i].length; j++) {
+//                 if (map[i][j] == 0) {
+//                     zeroCheck++;
+//                 }
+//             }
+//         }
+        
+//         if (zeroCheck > 0) {
+//             return false;
+//         }
+//         return true;
+//     }
+
+//     static boolean indexCheck(int r, int c) {
+//         return r >= 0 && r < map.length && c >= 0 && c < map[1].length;
+//     }
+
+
+
+    
+// }
+
+
+
+
+public class BJ7576 {
+    static int[][] map;
+    static int day = 0;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {-1, 1, 0, 0};
+    static int count;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] mapSize = br.readLine().split(" ");
+        Queue<Coordinate> queue = new LinkedList<>();
+
+        //맵 생성
+        map = new int[Integer.parseInt(mapSize[1])][Integer.parseInt(mapSize[0])];
+        for (int i = 0; i < map.length; i++) {
+            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        //최초 익은 과일 큐에 삽입
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] == 1) {
+                    queue.offer(new Coordinate(i, j, 0));
+                }
+            }
+        }
+
+
+
+        while (!queue.isEmpty()) {
+            Coordinate coordi = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                if (!indexCheck(coordi.x + dx[i], coordi.y + dy[i]) && (map[coordi.x + dx[i]][coordi.y + dy[i]] == 0)) {
+                    queue.offer(new Coordinate(coordi.x + dx[i], coordi.y + dy[i], coordi.day + 1));
+                    map[coordi.x + dx[i]][coordi.y + dy[i]] = 1;
+                }
+            }
+            day = coordi.day;
+        }
+
+
+        if (!checkZero()) {
+            day = -1;
+        }
+
+        System.out.println(day);
+
+    }
+
+    
+    // 좌표 클래스
+    public static class Coordinate {
+        public int x;
+        public int y;
+        public int day;
+
+        public Coordinate() {
+            super();
+        }
+
+        public Coordinate(int x, int y, int day) {
+            super();
+            this.x = x;
+            this.y = y;
+            this.day = day;
+        }
+    }
+
+    static boolean checkZero() {
+        int zeroCheck = 0;
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] == 0) {
+                    zeroCheck++;
+                }
+            }
+        }
+        
+        if (zeroCheck > 0) {
+            return false;
+        }
+        return true;
+    }
+
+    static boolean indexCheck(int r, int c) {
+        return r < 0 || r >= map.length || c < 0 || c >= map[1].length;
+    }
+}
