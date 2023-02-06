29e8e909af09cb3e91d07ad61c0ecf64242a528e
JeonJungyu <wjswnsrb93@naver.com>
2023-02-06 23:00:51 +0900

    feat: 백준 1074 - 재귀

---
+++ baekjoon/BJ1074.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1074 {
+    // static int[][] map;
+    static int count = 0;
+    static boolean isEnd = false;
+    static int nr;
+    static int nc;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        nr = Integer.parseInt(st.nextToken());
+        nc = Integer.parseInt(st.nextToken());
+        // map = new int[2<<N][2<<N];
+
+        // N == 1일 때 1 X 1 배열로 착각
+        // if (N == 1) {
+            // count = 0;
+        // } else {
+            exploreMap(0, 0, 1 << N );
+        // }
+           
+        System.out.println(count);
+    }
+
+    // 맵에 모두 담은 소스 / 시간 초과
+    // static void exploreMap(int r, int c, int n) {
+    //     if (n == 2) {
+    //         for (int i = 0; i < 2; i++) {
+    //             for (int j = 0; j < 2; j++) {
+    //                 map[r + i][c + j] = count++;
+    //             }
+    //         }
+    //     } else {
+    //         for (int i = 0; i < 2; i++) {      
+    //             for (int j = 0; j < 2; j++) {
+    //                 exploreMap(r + i * n / 2, c + j * n / 2, n / 2);
+    //             }
+    //         }
+    //     }
+    // }
+
+    static void exploreMap(int r, int c, int n) {
+        if (isEnd == true) return;
+        // System.out.printf("r: %d c:%d n:%d count:%d :: %d %d \n", r, c, n, count, nr - r, nc - c);
+        if (n == 1) {
+            a:
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    if (nr == r + i && nc == c + j) {
+                        isEnd = true;
+                        break a;
+                    } else {
+                        count++;
+                    }
+                }
+            }
+        } else {
+            a:
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    // 받은 값이 있는 범위
+                    if ((nr - r - i * n < n && nr - r - i * n >= 0) && (nc - c - j * n < n && nc - c - j * n >= 0) ) {
+                        exploreMap(r + i * n, c + j * n, n / 2);
+                        break a;
+                    } else {
+                        count += n * n;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ baekjoon/BJ1074_2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ1074_2 {
+    // static int[][] map;
+    static int count = 0;
+    static boolean isEnd = false;
+    static int nr;
+    static int nc;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        nr = Integer.parseInt(st.nextToken());
+        nc = Integer.parseInt(st.nextToken());
+        
+        exploreMap(0, 0, 1 << N );
+        
+
+    }
+
+    static void exploreMap(int r, int c, int width) {
+        if (nr == r && nc == c) {
+            System.out.println(count);
+            return;
+        }
+
+        if (r <= nr && r + width > nr && c <= nc && c + width > nc ) {
+            exploreMap(r, c, width/2);
+            exploreMap(r, c + width / 2, width/2);
+            exploreMap(r + width / 2, c, width/2);
+            exploreMap(r + width / 2, c + width / 2, width/2);
+        } else {
+            count += width * width;
+        }
+    }
+}
