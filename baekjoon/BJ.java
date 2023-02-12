9bd06faf6b0006b11b4f7c0c4449740beded81cf
JeonJungyu-1 <wjswnsrb93@naver.com>
2023-02-12 18:16:34 +0900

    feat: 백준 11403 경로 찾기 - 플로이드 워셜

--- baekjoon/BJ.java
+++ baekjoon/BJ.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 public class BJ {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
 
