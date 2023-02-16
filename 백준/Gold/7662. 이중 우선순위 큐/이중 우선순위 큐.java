d25489ed501140c536b15e7d0fddf36fbf59c6a2
JeonJungyu-1 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-02-16 16:43:11 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2280 ms, Memory: 440728 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -0,0 +1,58 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        TreeMap<Integer, Integer> treeMap;
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int testSize = Integer.parseInt(br.readLine());
+
+        for (int test_case = 0; test_case < testSize; test_case++) {
+            int N = Integer.parseInt(br.readLine());
+            treeMap = new TreeMap<>();
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                String mode = st.nextToken();
+                int num = Integer.parseInt(st.nextToken());
+                int numCount = -1;
+                
+                if (mode.equals("I")) {
+                    treeMap.put(num, treeMap.getOrDefault(num, 0) + 1);
+                } else {
+                    if (treeMap.isEmpty()) continue;
+                    num = num == -1 ? treeMap.firstKey() : treeMap.lastKey();
+                    numCount = treeMap.getOrDefault(num, -1);
+                    if (numCount > 0) {
+                        if (numCount == 1) {
+                            treeMap.remove(num);
+                        } else {
+                            treeMap.put(num, treeMap.getOrDefault(num, 0) - 1);
+                        }
+                    }
+                    // numCount = treeMap.put(num, treeMap.get(num) - 1);
+                    // if (numCount == 1) {
+                    //     treeMap.remove(num);
+
+                    // }
+                }
+
+                // sb.append(treeMap.keySet() + " : ; ;" + treeMap.entrySet() + "  ' df").append("\n");
+            } 
+
+            sb.append(treeMap.isEmpty() ? "EMPTY" : (treeMap.lastKey() + " " + treeMap.firstKey()));
+            if (test_case != testSize - 1) {
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+
+    }
+}
