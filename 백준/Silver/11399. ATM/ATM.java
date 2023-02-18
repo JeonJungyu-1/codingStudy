e27e4c1a3bdb614fa987c393f11553b921664202
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-02-18 16:58:10 +0900

    [Silver IV] Title: ATM, Time: 80 ms, Memory: 11848 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+// ATM
+// 1. 오름차순으로 정렬
+// 2. 차례대로 기다리는 시간 구해서 더하기
+public class Main {
+    static StringBuilder sb;
+    static int N;
+    static int[] time;
+    static int minTime;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
+    }
+    
+    private static void print() {
+        System.out.println(minTime);
+    }
+
+    private static void solve() {
+        Arrays.sort(time);
+        for (int i = 0; i < N; i++) {
+            minTime +=  (N - i) * time[i];
+        }
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        time = new int[N];
+        for (int i = 0; i < N; i++) {
+            time[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
