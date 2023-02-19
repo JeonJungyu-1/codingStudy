bcc70d881c9d1c7f7265990950aa56c95009c74c
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-02-19 14:06:18 +0900

    [Silver III] Title: 01타일, Time: 100 ms, Memory: 15456 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+// 01타일 - dp
+// 점화식 - f(1) = 1 / f(n) = f(n - 1) + f(n - 2)
+// top-down 사용 -- 재귀 많아서 불가능
+// bottom-up 사용
+public class Main {
+    static StringBuilder sb;
+    static int N;
+    static int[] memo;
+    static int result;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
+    }
+    
+    private static void print() {
+        System.out.println(result);
+    }
+
+    private static void solve() {
+        memo[1] = 1;
+        memo[2] = 2;
+        result = dp(N);
+    }
+
+    // top-down
+    // private static int dp(int n) {
+    //     if (n == 1) {
+    //         return 1;
+    //     } else if (n == 2) {
+    //         return 2;
+    //     }
+
+    //     if (memo[n] != 0) {
+    //         return memo[n];
+    //     } else {
+    //         memo[n] = dp(n - 2) + dp(n - 1);
+    //         if (memo[n] > 15746) memo[n] %= 15746;
+    //         return memo[n];
+    //     }
+    // }
+
+    // bottom-up
+    private static int dp(int n) {
+        if (memo[n] == 0) {
+            for (int i = 3; i <= n; i++) {
+                memo[i] = memo[i - 2] + memo[i - 1];
+                if (memo[i] >= 15746) memo[i] %= 15746;
+            }
+        }
+        return memo[n];
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        memo = new int[N + 2];
+
+
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
