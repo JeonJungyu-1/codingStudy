186f921b3119ca61cd843e3f09d2ec1d12ed0233
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-02-20 14:32:15 +0900

    [Silver I] Title: Z, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,74 +1,51 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Main {
-    // static int[][] map;
-    static int count = 0;
-    static boolean isEnd = false;
-    static int nr;
-    static int nc;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        nr = Integer.parseInt(st.nextToken());
-        nc = Integer.parseInt(st.nextToken());
-        // map = new int[2<<N][2<<N];
-        // if (N == 1) {
-            // count = 0;
-        // } else {
-            exploreMap(0, 0, 1 << N );
-        // }
-           
-        System.out.println(count);
+    static int count;
+    static int N;
+    static int R, C;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
     }
 
-    // static void exploreMap(int r, int c, int n) {
-    //     if (n == 2) {
-    //         for (int i = 0; i < 2; i++) {
-    //             for (int j = 0; j < 2; j++) {
-    //                 map[r + i][c + j] = count++;
-    //             }
-    //         }
-    //     } else {
-    //         for (int i = 0; i < 2; i++) {      
-    //             for (int j = 0; j < 2; j++) {
-    //                 exploreMap(r + i * n / 2, c + j * n / 2, n / 2);
-    //             }
-    //         }
-    //     }
-    // }3 7 7
+    private static void print() {
+    }
 
-    static void exploreMap(int r, int c, int n) {
-        if (isEnd == true) return;
-        // System.out.printf("r: %d c:%d n:%d count:%d :: %d %d \n", r, c, n, count, nr - r, nc - c);
-        if (n == 1) {
-            a:
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 2; j++) {
-                    if (nr == r + i && nc == c + j) {
-                        isEnd = true;
-                        break a;
-                    } else {
-                        count++;
-                    }
-                }
-            }
-        } else {
-            a:
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 2; j++) {
-                    // if ((nr - i * n >= r + i || nr - i * n < 0) || (nc - j * n >= c + j || nc - j * n < 0)) {
-                    if ((nr - r - i * n < n && nr - r - i * n >= 0) && (nc - c - j * n < n && nc - c - j * n >= 0) ) {
-                        exploreMap(r + i * n, c + j * n, n / 2);
-                        break a;
-                    } else {
-                        count += n * n;
-                    }
-                }
-            }
+    private static void solve() {
+        divide(0, 0, N);
+    }
 
+    private static void divide(int r, int c, int size) {
+
+        if (r == R && c == C) {
+            System.out.println(count);
+            return;
         }
+            
+        if (r <= R && r + size > R && c <= C && c + size > C) {
+            int half = size/2;
+            divide(r, c, half);
+            divide(r, c + half, half);
+            divide(r + half, c, half);
+            divide(r + half, c + half, half);
+        } else {
+            count += size * size;
+        }
+        
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        N = 1 << N;
     }
 }
