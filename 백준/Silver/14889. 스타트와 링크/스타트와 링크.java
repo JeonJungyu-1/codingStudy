928d24536c4a113a1b8f72b6e0d66bcfdb0ec068
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-02-21 22:38:35 +0900

    [Silver II] Title: 스타트와 링크, Time: 368 ms, Memory: 15392 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 스타트와 링크
+// 1. nCn/2로 조합을 구해서 스타트팀과 링크팀을 나누기
+// 2. 나눈 팀을 가지고 다시 n/2C2을 통해 능력치 더해주기
+
+public class Main {
+    static StringBuilder sb;
+    static int N;
+    static int[][] map;
+    static boolean[] isVisited;
+    static int min;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
+    }
+    
+    private static void print() {
+        System.out.println(min);
+    }
+
+    private static void solve() {
+        combination(0, 0);
+
+    }
+
+    private static void combination(int cnt, int start) {
+        if (cnt == N/2) {
+            calStatDiff();
+        }
+
+        for (int i = start; i < N; i++) {
+            isVisited[i] = true;
+            combination(cnt + 1, i + 1);
+            isVisited[i] = false;
+        }
+    
+    }
+
+    private static void calStatDiff() {
+        int start = 0;
+        int link = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (i != j) {
+                    if (isVisited[i] && isVisited[j]) {
+                        start += map[i][j];
+                        start += map[j][i];
+                    } else if (!isVisited[i] && !isVisited[j]) {
+                        link += map[i][j];
+                        link += map[j][i];
+                    }
+                }
+            }
+        }
+
+        min = Math.min(min, Math.abs(start - link));
+        
+
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        isVisited = new boolean[N];
+        min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+    }
+}
