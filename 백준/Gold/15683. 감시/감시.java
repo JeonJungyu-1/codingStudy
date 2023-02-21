2062e573425dc643ef9c48d24fc58484547af1ae
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-02-21 15:47:18 +0900

    [Gold IV] Title: 감시, Time: 260 ms, Memory: 84656 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -42,34 +42,20 @@ public class Main {
         Node node = list.get(cnt);
 
         switch (node.num) {
-            case 1:
-                explore(cnt + 1, mapCopy(m, node, 0));
-                explore(cnt + 1, mapCopy(m, node, 1));
-                explore(cnt + 1, mapCopy(m, node, 2));
-                explore(cnt + 1, mapCopy(m, node, 3));
-                break;
             case 2:
-            explore(cnt + 1, mapCopy(m, node, 0));
-            explore(cnt + 1, mapCopy(m, node, 1));
-                
+                for (int i = 0; i < 2; i++) {
+                    explore(cnt + 1, mapCopy(m, node, i));
+                }
                 break;
+            case 1:
             case 3:
-            explore(cnt + 1, mapCopy(m, node, 0));
-            explore(cnt + 1, mapCopy(m, node, 1));
-            explore(cnt + 1, mapCopy(m, node, 2));
-            explore(cnt + 1, mapCopy(m, node, 3));
-                
-                break;
             case 4:
-            explore(cnt + 1, mapCopy(m, node, 0));
-            explore(cnt + 1, mapCopy(m, node, 1));
-            explore(cnt + 1, mapCopy(m, node, 2));
-            explore(cnt + 1, mapCopy(m, node, 3));
-                
+                for (int i = 0; i < 4; i++) {
+                    explore(cnt + 1, mapCopy(m, node, i));
+                }
                 break;
             case 5:
-            explore(cnt + 1, mapCopy(m, node, 0));                
-                
+                explore(cnt + 1, mapCopy(m, node, 0));                
                 break;
         }
 
@@ -88,209 +74,69 @@ public class Main {
         switch (node.num) {
             case 1:
                 if (cnt == 0) { // 오른쪽
-                    for (int i = node.c + 1; i < M; i++ ) {
-                        if (copyMap[node.r][i] == 6) break;
-                        if (copyMap[node.r][i] != 0) continue;
-                        copyMap[node.r][i] = -1;
-                    }
+                    right(node, copyMap);
                 } else if (cnt == 1) { // 왼쪽
-                    for (int i = node.c - 1; i >= 0; i-- ) {
-                        if (copyMap[node.r][i] == 6) break; 
-                        if (copyMap[node.r][i] != 0) continue;
-                        copyMap[node.r][i] = -1;
-                    }
+                    left(node, copyMap);
                 } else if (cnt == 2) { // 위쪽
-                    for (int i = node.r - 1; i >= 0; i-- ) {
-                        if (copyMap[i][node.c] == 6) break;  
-                        if (copyMap[i][node.c] != 0) continue;
-                        copyMap[i][node.c] = -1;
-                    }
+                    up(node, copyMap);
                 } else if (cnt == 3) { // 아래쪽
-                    for (int i = node.r + 1; i < N; i++ ) {
-                        if (copyMap[i][node.c] == 6) break;  
-                        if (copyMap[i][node.c] != 0) continue;
-                        copyMap[i][node.c] = -1;
-                    }
+                    down(node, copyMap);
                 }
                 
                 break;
             case 2:
                 if (cnt == 0) { // 좌우
-                    for (int i = node.c + 1; i < M; i++ ) {
-                        if (copyMap[node.r][i] == 6) break; 
-                        if (copyMap[node.r][i] != 0) continue;
-                        copyMap[node.r][i] = -1;
-                    }
-                    for (int i = node.c - 1; i >= 0; i-- ) {
-                        if (copyMap[node.r][i] == 6) break; 
-                        if (copyMap[node.r][i] != 0) continue;
-                        copyMap[node.r][i] = -1;
-                    }
+                    right(node, copyMap);
+                    left(node, copyMap);
                 } else if (cnt == 1) { // 위아래
-                    for (int i = node.r - 1; i >= 0; i-- ) {
-                        if (copyMap[i][node.c] == 6) break;  
-                        if (copyMap[i][node.c] != 0) continue;
-                        copyMap[i][node.c] = -1;
-                    }
-                    for (int i = node.r + 1; i < N; i++ ) {
-                        if (copyMap[i][node.c] == 6) break;  
-                        if (copyMap[i][node.c] != 0) continue;
-                        copyMap[i][node.c] = -1;
-                    }
+                    up(node, copyMap);
+                    down(node, copyMap);
                 }
                 
                 break;
             case 3:
                 if (cnt == 0) { // 위 오
-                        for (int i = node.c + 1; i < M; i++ ) {
-                            if (copyMap[node.r][i] == 6) break; 
-                            if (copyMap[node.r][i] != 0) continue;
-                            copyMap[node.r][i] = -1;
-                        }
-                    
-                        for (int i = node.r - 1; i >= 0; i-- ) {
-                            if (copyMap[i][node.c] == 6) break;  
-                            if (copyMap[i][node.c] != 0) continue;
-                            copyMap[i][node.c] = -1;
-                        }
-                    
+                        right(node, copyMap);
+                        up(node, copyMap);
                 } else if (cnt == 1) { // 오 아
-                        for (int i = node.c + 1; i < M; i++ ) {
-                            if (copyMap[node.r][i] == 6) break; 
-                            if (copyMap[node.r][i] != 0) continue;
-                            copyMap[node.r][i] = -1;
-                        }
-                    
-                        for (int i = node.r + 1; i < N; i++ ) {
-                            if (copyMap[i][node.c] == 6) break;  
-                            if (copyMap[i][node.c] != 0) continue;
-                            copyMap[i][node.c] = -1;
-                        }
-                    
-
+                        right(node, copyMap);
+                        down(node, copyMap);
                 } else if (cnt == 2) { // 왼 위
-
-              
-                        for (int i = node.c - 1; i >= 0; i-- ) {
-                            if (copyMap[node.r][i] == 6) break; 
-                            if (copyMap[node.r][i] != 0) continue;
-                            copyMap[node.r][i] = -1;
-                        }
-                        for (int i = node.r - 1; i >= 0; i-- ) {
-                            if (copyMap[i][node.c] == 6) break;  
-                            if (copyMap[i][node.c] != 0) continue;
-                            copyMap[i][node.c] = -1;
-                        }
+                        left(node, copyMap);
+                        up(node, copyMap);
                    
                 } else if (cnt == 3) { // 왼 아
-                    
-                        for (int i = node.c - 1; i >= 0; i-- ) {
-                            if (copyMap[node.r][i] == 6) break; 
-                            if (copyMap[node.r][i] != 0) continue;
-                            copyMap[node.r][i] = -1;
-                        }
-                   
-                        for (int i = node.r + 1; i < N; i++ ) {
-                            if (copyMap[i][node.c] == 6) break;  
-                            if (copyMap[i][node.c] != 0) continue;
-                            copyMap[i][node.c] = -1;
-                        }
-                    
+                        left(node, copyMap);
+                        down(node, copyMap);
                 }
                 break;
             case 4:
                 if (cnt == 0) {
-                        for (int i = node.c + 1; i < M; i++ ) {
-                            if (copyMap[node.r][i] == 6) break; 
-                            if (copyMap[node.r][i] != 0) continue;
-                            copyMap[node.r][i] = -1;
-                        }
-                        for (int i = node.c - 1; i >= 0; i-- ) {
-                            if (copyMap[node.r][i] == 6) break; 
-                            if (copyMap[node.r][i] != 0) continue;
-                            copyMap[node.r][i] = -1;
-                        }
-                        for (int i = node.r - 1; i >= 0; i-- ) {
-                            if (copyMap[i][node.c] == 6) break;  
-                            if (copyMap[i][node.c] != 0) continue;
-                            copyMap[i][node.c] = -1;
-                        }
+                        right(node, copyMap);
+                        left(node, copyMap);
+                        up(node, copyMap);
                     
                 } else if (cnt == 1) {
-                        for (int i = node.c + 1; i < M; i++ ) {
-                            if (copyMap[node.r][i] == 6) break; 
-                            if (copyMap[node.r][i] != 0) continue;
-                            copyMap[node.r][i] = -1;
-                        }
-                        for (int i = node.c - 1; i >= 0; i-- ) {
-                            if (copyMap[node.r][i] == 6) break; 
-                            if (copyMap[node.r][i] != 0) continue;
-                            copyMap[node.r][i] = -1;
-                        }
-                    
-                        for (int i = node.r + 1; i < N; i++ ) {
-                            if (copyMap[i][node.c]== 6) break;
-                            copyMap[i][node.c] = -1;
-                        }
+                        right(node, copyMap);
+                        left(node, copyMap);
+                        down(node, copyMap);
 
                 } else if (cnt == 2) {
-                        for (int i = node.c + 1; i < M; i++ ) {
-                            if (copyMap[node.r][i] == 6) break; 
-                            if (copyMap[node.r][i] != 0) continue;
-                            copyMap[node.r][i] = -1;
-                        }
-                        
-                        for (int i = node.r - 1; i >= 0; i-- ) {
-                            if (copyMap[i][node.c] == 6) break;  
-                            if (copyMap[i][node.c] != 0) continue;
-                            copyMap[i][node.c] = -1;
-                        }
-                        for (int i = node.r + 1; i < N; i++ ) {
-                            if (copyMap[i][node.c] == 6) break;  
-                            if (copyMap[i][node.c] != 0) continue;
-                            copyMap[i][node.c] = -1;
-                        }
+                        right(node, copyMap);
+                        up(node, copyMap);
+                        down(node, copyMap);
 
                 } else if (cnt == 3) {
-                        
-                        for (int i = node.c - 1; i >= 0; i-- ) {
-                            if (copyMap[node.r][i] == 6) break; 
-                            if (copyMap[node.r][i] != 0) continue;
-                            copyMap[node.r][i] = -1;
-                        }
-                        for (int i = node.r - 1; i >= 0; i-- ) {
-                            if (copyMap[i][node.c] == 6) break;  
-                            if (copyMap[i][node.c] != 0) continue;
-                            copyMap[i][node.c] = -1;
-                        }
-                        for (int i = node.r + 1; i < N; i++ ) {
-                            if (copyMap[i][node.c] == 6) break;  
-                            if (copyMap[i][node.c] != 0) continue;
-                            copyMap[i][node.c] = -1;
-                        }
+                        left(node, copyMap);
+                        up(node, copyMap);
+                        down(node, copyMap);
                 }
                 break;
             case 5:
-                for (int i = node.c + 1; i < M; i++ ) {
-                    if (copyMap[node.r][i] == 6) break; 
-                    if (copyMap[node.r][i] != 0) continue;
-                    copyMap[node.r][i] = -1;
-                }
-                for (int i = node.c - 1; i >= 0; i-- ) {
-                    if (copyMap[node.r][i] == 6) break; 
-                    if (copyMap[node.r][i] != 0) continue;
-                    copyMap[node.r][i] = -1;
-                }
-                for (int i = node.r - 1; i >= 0; i-- ) {
-                    if (copyMap[i][node.c]== 6) break;
-                    copyMap[i][node.c] = -1;
-                }
-                for (int i = node.r + 1; i < N; i++ ) {
-                    if (copyMap[i][node.c] == 6) break;  
-                    if (copyMap[i][node.c] != 0) continue;
-                    copyMap[i][node.c] = -1;
-                }
-            
+                right(node, copyMap);
+                left(node, copyMap);
+                down(node, copyMap);
+                up(node, copyMap);
                 
                 break;
 
@@ -300,12 +146,31 @@ public class Main {
 
         return copyMap;
     }
-
-    private static boolean mapCheck(int r, int c) {
-        return r >= 0 && r < N && c >= 0 && c < M;
+    private static void down(Node node, int[][] copyMap) {
+        for (int i = node.r + 1; i < N; i++ ) {
+            if (copyMap[i][node.c] == 6) break;  
+            copyMap[i][node.c] = -1;
+        }
+    }
+    private static void up(Node node, int[][] copyMap) {
+        for (int i = node.r - 1; i >= 0; i-- ) {
+            if (copyMap[i][node.c] == 6) break;  
+            copyMap[i][node.c] = -1;
+        }
+    }
+    private static void left(Node node, int[][] copyMap) {
+        for (int i = node.c - 1; i >= 0; i-- ) {
+            if (copyMap[node.r][i] == 6) break; 
+            copyMap[node.r][i] = -1;
+        }
+    }
+    private static void right(Node node, int[][] copyMap) {
+        for (int i = node.c + 1; i < M; i++ ) {
+            if (copyMap[node.r][i] == 6) break;
+            copyMap[node.r][i] = -1;
+        }
     }
 
-   
 
     private static void calMap(int[][] m) {
         int cnt = 0;
@@ -316,15 +181,7 @@ public class Main {
                 }
             }
         }
-
         
-        // for (int i = 0; i < N; i++) {
-        //     for (int j = 0; j < M; j++) {
-        //         System.out.print(m[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        // System.out.println();
         min = Math.min(min, cnt);
 
     }
