a935ef309661472a94adb7a345380f9ec47242c3
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-02-24 14:12:27 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 145 ms, Memory: 25,032 KB -BaekjoonHub

--- SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
+++ SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
@@ -1,29 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
     static int T, M, C;
-    static int[] A, B;
-    static Node aNode, bNode;
+    static Node[] A, B;
     static chargeH[] chargeList;
     static int[] dr = {0,-1, 0, 1, 0};
     static int[] dc = {0, 0, 1, 0, -1};
-    static int aSum, bSum;
-    static int[][][] map;
-    static int[][] aMoveMap;
-    static int[][] bMoveMap;
+    static int sum;
+    static int[][] map;
     static boolean[][] isVisited;
-    static List<Same> aSame;
-    static List<Same> bSame;
     
 
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -39,287 +30,108 @@ public class Solution {
             StringTokenizer st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             C = Integer.parseInt(st.nextToken());
-            A = new int[M];
-            B = new int[M];
+            A = new Node[M + 1];
+            B = new Node[M + 1];
             chargeList = new chargeH[C + 1];
 
+            
+            Node aNode = new Node(0, 0);
+            Node bNode = new Node(9, 9);
+
+            A[0] = new Node(aNode.r, aNode.c);
+            B[0] = new Node(bNode.r, bNode.c);
+
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < M; i++) {
-                A[i] = Integer.parseInt(st.nextToken());
+            for (int i = 1; i < M + 1; i++) {
+                int d = Integer.parseInt(st.nextToken());
+                aNode.r += dr[d];
+                aNode.c += dc[d];
+                A[i] = new Node(aNode.r, aNode.c);
             }
 
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < M; i++) {
-                B[i] = Integer.parseInt(st.nextToken());
+            for (int i = 1; i < M + 1; i++) {
+                int d = Integer.parseInt(st.nextToken());
+                bNode.r += dr[d];
+                bNode.c += dc[d];
+                B[i] = new Node(bNode.r, bNode.c);
             }
 
             for (int i = 1; i < C + 1; i++) {
                 st = new StringTokenizer(br.readLine());
                 chargeList[i] = new chargeH(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
             }
-            map = new int[C + 1][10][10];
-            aMoveMap = new int[10][10];
-            bMoveMap = new int[10][10];
-            aNode = new Node(0, 0);
-            bNode = new Node(9, 9);
-            aSame = new ArrayList<>();
-            bSame = new ArrayList<>();
 
-            aSum = 0;
-            bSum = 0;
+            sum = 0;
 
             solve();
 
-            sb.append("#").append(t).append(" ").append(aSum + bSum).append("\n");
+            sb.append("#").append(t).append(" ").append(sum).append("\n");
         }
         System.out.println(sb.toString());
     }
 
     // 충전범위 3차원 배열에 각각 저장
     private static void solve() {
-        exploreCharge();
-        // move();
-
-
-        // for (int i = 0; i < same.size(); i++) {
-        //     Node node = same.get(i);
-        // }
-
-        // for (Node[] a: map) {
-        //     for (Node n: a) {
-        //         System.out.print(n);
-        //     }
-        //     System.out.println();
-        //     // System.out.println(Arrays.toString(a));
-        // } 
-
-        // for (int[] q: aMoveMap) {
-        //     System.out.println(Arrays.toString(q));
-        // }
-        // System.out.println();
-
-        // for (int[] w: bMoveMap) {
-        //     System.out.println(Arrays.toString(w));
-        // }
-
-        
-
-        // for (int[][] a: map) {
-        //     for (int[] b: a) {
-        //         System.out.println(Arrays.toString(b));
-        //     }
-        //     System.out.println();
-        // }
-        
-        // Collections.sort(aSame);
-        // Collections.sort(bSame);
-        // for (int i = 0; i < aSame.size(); i++) {
-        //     for (int j = 0; j < bSame.size(); j++) {
-        //         if (aSame.get(i).t == bSame.get(j).t) {
-        //             if (i + 1 < aSame.size() && aSame.get(i).t == aSame.get(i + 1).t) {
-                        
-        //             }
-        //             if (j + 1 < bSame.size() && bSame.get(i).t == bSame.get(i + 1).t) {
-
-        //             }
-        //         }
-        //     }
-        // }
-        // for (Same s: aSame) {
-        //     System.out.println(s);
-        // }
+        // setCharge();
         explore();
-        // System.out.println(aSum);
-        // System.out.println(bSum);
-        
-        
     }
 
-    private static void move() {
-        aMoveMap[aNode.r][aNode.c] = 1;
-        bMoveMap[bNode.r][bNode.c] = 1;
-        for (int i = 0; i < M; i++) {
-            
-            aNode.r += dr[A[i]];
-            aNode.c += dc[A[i]];
-            if (aMoveMap[aNode.r][aNode.c] == 0) {
-                aMoveMap[aNode.r][aNode.c] = i + 1;
-            }
 
-            bNode.r += dr[B[i]];
-            bNode.c += dc[B[i]];
-            if (bMoveMap[bNode.r][bNode.c] == 0) {
-                bMoveMap[bNode.r][bNode.c] = i + 1;
-            }
-        }
-        
-
-    }
-
+    // 한칸 갈 때마다 모든 충전기의 범위와 거리계산 해보기
     private static void explore() {
-        int aNr = aNode.r;
-        int aNc = aNode.c;
+        List<chargeType> currentCharge = new ArrayList<>();
+        for (int i = 0; i < M + 1; i++) {
+            // System.out.println(sum);
+            // System.out.println(A[i]);
+            currentCharge.clear();
 
-        int bNr = bNode.r;
-        int bNc = bNode.c;
-        
-        List<chargeType> a = new ArrayList<>();
-        List<chargeType> b = new ArrayList<>();
-        for (int i = 0; i <= M; i++) {
-            a.clear();
-            b.clear();
-
-            
             for (int j = 1; j < chargeList.length; j++) {
-                if (map[j][aNr][aNc] > 0) {
-                    a.add(new chargeType(j, map[j][aNr][aNc]));
-                }
+                if (calDistance(A[i].r, A[i].c, chargeList[j].r, chargeList[j].c) > chargeList[j].distance) continue;
+                currentCharge.add(new chargeType(1, chargeList[j].ch, j));
             }
-            
             for (int j = 1; j < chargeList.length; j++) {
-                if (map[j][bNr][bNc] > 0) {
-                    b.add(new chargeType(j, map[j][bNr][bNc]));
-                }
+                if (calDistance(B[i].r, B[i].c, chargeList[j].r, chargeList[j].c) > chargeList[j].distance) continue;
+                currentCharge.add(new chargeType(2, chargeList[j].ch, j));
             }
 
-            Collections.sort(a, Comparator.reverseOrder());
-            Collections.sort(b, Comparator.reverseOrder());
-
-            if (a.size() == 1 && b.size() == 1) {
-                if (a.get(0).bc != b.get(0).bc) {
-                    aSum += a.get(0).ch;
-                    bSum += b.get(0).ch;
-                    
-                } else {
-                    aSum += a.get(0).ch/2;
-                    bSum += b.get(0).ch/2;
-
-                }
-            } else if (a.size() == 0 && b.size() >= 1) {
-                bSum += b.get(0).ch;
-            } else if (a.size() >= 1 && b.size() == 0) { 
-                aSum += a.get(0).ch;
-
-            } else if (a.size() >= 2 || b.size() >= 2) { 
-                int m = Integer.MIN_VALUE;
-                for (int j = 0; j < a.size(); j++) {
-                    for (int k = 0; k < b.size(); k++) {
-                        if (a.get(j).bc == b.get(k).bc) {
-                            m = Math.max(m, a.get(j).ch);
+            int max = Integer.MIN_VALUE;
+            if (currentCharge.size() == 1) {
+                sum += currentCharge.get(0).ch;
+            }  else if (currentCharge.size() > 1) {
+                for (int j = 0; j < currentCharge.size(); j++) {
+                    for (int k = j + 1; k < currentCharge.size(); k++) {
+                        if (j == k) continue;
+                        chargeType a = currentCharge.get(j);
+                        chargeType b = currentCharge.get(k);
+                        if (a.who != b.who) {
+                            if (a.ch == b.ch) {
+                                if (a.index == b.index) {
+                                    max = Math.max(max, a.ch);
+                                } else {
+                                    max = Math.max(max, a.ch + b.ch);
+                                }
+                            } else {
+                                max = Math.max(max, a.ch + b.ch);
+                            }
                         } else {
-                            m = Math.max(m, a.get(j).ch + b.get(k).ch);
+                            max = Math.max(max, Math.max(a.ch, b.ch));
                         }
                     }
                 }
-
-                aSum += m;
-            }  
-
-            // System.out.println(aSum);
-            // System.out.println(bSum);
-            // System.out.println(aSum);
-            // System.out.println(bSum);
-
-            // if (a.size() >= 1) {
-            //     Collections.sort(a, Comparator.reverseOrder());
-            //     aSum += a.get(0);
-            //     a.clear();
-            // }
-            // if (b.size() >= 1) {
-            //     Collections.sort(b, Comparator.reverseOrder());
-            //     bSum += b.get(0);
-            //     b.clear();
-            // }
-
-
-            if (i < M) {
-                aNr += dr[A[i]];
-                aNc += dc[A[i]];
-    
-                bNr += dr[B[i]];
-                bNc += dc[B[i]];
+                sum += max;
             }
-        }
         
 
-    }
-
-    private static void exploreCharge() {
-        for (int i = 1; i < C + 1; i++) {
-            bfs(i);
         }
     }
 
-    private static void bfs(int index) {
-        isVisited = new boolean[10][10];
-        Queue<Node> que = new ArrayDeque<>();
-        chargeH ch = chargeList[index];
-        que.offer(new Node(ch.r, ch.c, 0));
-        isVisited[ch.r][ch.c] = true;
-        map[index][ch.r][ch.c] = ch.ch;
-
-        while (!que.isEmpty()) {
-            Node node = que.poll();
-
-            
-                // if (aMoveMap[node.r][node.c] > 0) {
-                //     aSum += ch.ch;
-                //     aSame.add(new Same(aMoveMap[node.r][node.c], ch.ch));
-                //     // map[node.r][node.c] = 1;
-                // }
-                // if (bMoveMap[node.r][node.c] > 0) {
-                //     bSum += ch.ch;
-                //     bSame.add(new Same(bMoveMap[node.r][node.c], ch.ch));
-                //     // map[node.r][node.c] = 2;
-                // }
-
-            
-            
-            // if (moveMap[node.r][node.c] == 3) {
-            //     aSum += ch.ch/2;
-            //     bSum += ch.ch/2;
-            //     moveMap[node.r][node.c] = 3;
-            // }
-
-            for (int i = 1; i < 5; i++) {
-                int nr = node.r + dr[i];
-                int nc = node.c + dc[i];
-                if (!mapcheck(nr, nc) || isVisited[nr][nc]) continue;
-                if (calDistance(ch.r, ch.c, nr, nc) > ch.distance) continue;
-                que.offer(new Node(nr, nc));
-                isVisited[nr][nc] = true;
-                map[index][nr][nc] = ch.ch;
-                
-            }
-        }
-        
-    }
-
-    private static boolean mapcheck(int r, int c) {
-        return r >= 0 && r < 10 && c >= 0 && c < 10;
-    }
 
     private static int calDistance(int r, int c, int nr, int nc) {
         return Math.abs(r - nr) + Math.abs(c - nc);
     }
 
-    static class Same {
-        int t;
-        int ch;
-        public Same(int t, int ch) {
-            this.t = t;
-            this.ch = ch;
-        }
-        @Override
-        public String toString() {
-            return "Same [t=" + t + ", ch=" + ch + "]";
-        }
 
-
-  
-
-        
-    }
 
     static class Node {
         int r;
@@ -338,25 +150,34 @@ public class Solution {
             this.c = c;
             this.ch = ch;
         }
+
+
+        @Override
+        public String toString() {
+            return "Node [r=" + r + ", c=" + c + ", ch=" + ch + "]";
+        }
+
+        
         
     }
-
     static class chargeType implements Comparable<chargeType> {
-        int bc;
+        int who;
         int ch;
-        public chargeType(int bc, int ch) {
-            this.bc = bc;
+        int index;
+        public chargeType(int who, int ch, int index) {
+            this.who = who;
             this.ch = ch;
+            this.index = index;
         }
         @Override
         public int compareTo(chargeType o) {
-            // TODO Auto-generated method stub
-            return this.ch - o.ch;
+            return Integer.compare(this.who, o.who);
         }
 
         
         
     }
+
 
     static class chargeH {
         int r;
