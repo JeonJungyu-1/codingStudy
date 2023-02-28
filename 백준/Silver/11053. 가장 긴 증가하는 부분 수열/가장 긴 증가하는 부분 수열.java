6f5a43e3fef73705fc0cee5511f0c79d8ffa1caa
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-02-28 21:29:03 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 100 ms, Memory: 11896 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 가장 긴 증가하는 부분 수열
+// LIS
+
+public class Main {
+    static StringBuilder sb;
+    static int N;
+    static int[] numbers;
+    static int[] dp;
+    static int maxLis;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
+    }
+    
+    private static void print() {
+        System.out.println(maxLis);
+    }
+
+    private static void solve() {
+
+        maxLis = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (numbers[j] < numbers[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLis = Math.max(maxLis, dp[i]);
+        }
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        numbers = new int[N];
+        dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
