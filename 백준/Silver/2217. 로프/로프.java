dedffb51d599744fe62ac6466e29b193c692199c
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-03-01 17:41:39 +0900

    [Silver IV] Title: 로프, Time: 296 ms, Memory: 26188 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 로프
+// 그리디
+
+public class Main {
+    static StringBuilder sb;
+    static int N;
+    static int[] ropeList;
+    static int maxWeight;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
+    }
+    
+    private static void print() {
+        System.out.println(maxWeight);
+    }
+
+    private static void solve() {
+        Arrays.sort(ropeList);
+        maxWeight = Integer.MIN_VALUE;
+        for (int i = 0; i < ropeList.length; i++) {
+            maxWeight = Math.max(maxWeight, ropeList[i] * (ropeList.length - i));
+        }
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        ropeList = new int[N];
+        for (int i = 0; i < N; i++) {
+            ropeList[i] = Integer.parseInt(br.readLine());
+        }
+
+    }
+}
