fe8893649883906588a6fd856dde52c81aeb115d
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-03-02 14:28:32 +0900

    [Gold IV] Title: 최단경로, Time: 660 ms, Memory: 116460 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -4,19 +4,28 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int[] distance;
     static int V, E, K;
+    static PriorityQueue<Edge> pq;
 
-    static class Edge {
+    static class Edge implements Comparable<Edge> {
         int v, w;
 
         public Edge(int v, int w) {
             this.v = v;
             this.w = w;
         }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.w, o.w);
+        }
+
+        
     }
 
     static List<Edge>[] adj;
@@ -28,6 +37,7 @@ public class Main {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(br.readLine());
+        pq = new PriorityQueue<>();
 
         adj = new ArrayList[V];
         for (int i = 0; i < V; i++) {
@@ -46,26 +56,36 @@ public class Main {
         checked = new boolean[V];
 
         distance[K - 1] = 0;
+        pq.offer(new Edge(K - 1, 0));
 
-        for (int i = 0; i < V - 1; i++) {
-            int minV = MM;
-            int w = -1;
-            for (int j = 0; j < V; j++) {
-                if (!checked[j] && minV > distance[j]) {
-                    minV = distance[j];
-                    w = j;
-                }
-            }
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            if (checked[edge.v]) continue;
+            checked[edge.v] = true;
 
-            if (w == -1) break; // 끊어짐
-            checked[w] = true;
-
-            for (Edge next: adj[w]) {
-                if (!checked[next.v] && distance[next.v] > distance[w] + next.w) {
-                    distance[next.v] = distance[w] + next.w;
+            for (Edge next: adj[edge.v]) {
+                if (!checked[next.v] && distance[next.v] > distance[edge.v] + next.w) {
+                    distance[next.v] = distance[edge.v] + next.w;
+                    pq.offer(new Edge(next.v, distance[next.v]));
                 }
             }
         }
+
+        // for (int i = 0; i < V - 1; i++) {
+        //     int minV = MM;
+        //     int w = -1;
+        //     for (int j = 0; j < V; j++) {
+        //         if (!checked[j] && minV > distance[j]) {
+        //             minV = distance[j];
+        //             w = j;
+        //         }
+        //     }
+
+        //     if (w == -1) break; // 끊어짐
+        //     checked[w] = true;
+
+            
+        // }
 
         StringBuilder sb = new StringBuilder();
         for (int d : distance) {
