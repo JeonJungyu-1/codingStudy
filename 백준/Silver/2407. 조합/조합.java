b51f47c92577461b273e7266c55a3d491ff724d7
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-03-05 18:05:35 +0900

    [Silver III] Title: 조합, Time: 80 ms, Memory: 11660 KB -BaekjoonHub

---
+++ 백준/Silver/2407. 조합/조합.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+// 조합
+
+public class Main {
+    static StringBuilder sb;
+    static long N, M;
+    static BigInteger result;
+    static BigInteger divideNumber;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
+    }
+    
+    private static void print() {
+        System.out.println(result);
+    }
+
+    private static void solve() {
+        result = new BigInteger("1");
+        divideNumber = new BigInteger("1");
+        for (long i = 0L; i < M; i++) {
+            result = result.multiply(BigInteger.valueOf(N - i));
+            divideNumber = divideNumber.multiply(BigInteger.valueOf(i + 1));
+        }
+
+        result = result.divide(divideNumber);
+
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        N = Long.parseLong(st.nextToken());
+        M = Long.parseLong(st.nextToken());
+    }
+}
