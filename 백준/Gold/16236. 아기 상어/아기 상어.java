5908a2653cf5057236fa1ba4940142717f9599ad
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-03-06 13:52:58 +0900

    [Gold III] Title: 아기 상어, Time: 112 ms, Memory: 16340 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,170 +1,131 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-// 아기상어
 public class Main {
+	static int N;
+	static int[][] map;
+	static int[] start;
+	static int[] dr = {-1, 0, 0, 1};
+	static int[] dc = {0, -1, 1, 0};
+	static boolean[][] visited;
+	static int shark = 2;
+	static int eatCount = 0;
+	static int result = 0;
+	static PriorityQueue<Shark> pq = new PriorityQueue<>();
+	
+	static class Shark implements Comparable<Shark> {
+		int r;
+		int c;
+		int time;
+		
+		public Shark(int r, int c, int time) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.time = time;
+		}
 
-    static int N;
-    static int map[][];
-    static int[][] isVisited;
-    static Queue<int[]> queue;
-    static int shark;
-    static int time;
-    static int totalTime;
-    static int eat;
-    static int[] min;
-    // 거리가 가깝다면 위, 여러마리 있다면 왼쪽 이니 이동은 위 왼 먼저 하기
-    static int[] dr = {-1, 0, 0, 1};
-    static int[] dc = {0, -1, 1, 0};
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        queue = new ArrayDeque<>();
+		@Override
+		public int compareTo(Shark o) {
+			if(this.time == o.time) {
+				if(this.r == o.r) {
+					return Integer.compare(this.c, o.c);
+				}
+				return Integer.compare(this.r, o.r);
+			}
+			return Integer.compare(this.time, o.time);
+		}
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		start = new int[2];
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 9) {
+					
+					start[0] = i;
+					start[1] = j;
+				}
+			}
+		}
+				
+		while(bfs()) {
+			Shark now = pq.poll();
+			map[now.r][now.c] = 0;
+			start[0] = now.r;
+			start[1] = now.c;
+			
+			result += now.time;
+			eatCount++;
+			
+			if(eatCount == shark) {
+				shark++;
+				eatCount = 0;
+			}
+			
+			pq.clear();
+		}
+		
+		System.out.println(result);
+	}
+	
+	// 일단 먹을수 있는 애가 있는지 없는지 체크, 가장 가까운 애부터 먹음
+	// bfs로 거리 재면서 가장 가까운 애 먹고, 상어 위치를 거기로 갱신
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 9) {
-                    queue.add(new int[]{i, j});
-                    map[i][j] = 0;
-                }
-            }
-        }
-        shark = 2;
-        time = 0;
-        totalTime = 0;
-        eat = 0;
-        
-        while (true) {
-            
-            isVisited = new int[N][N];
-            min = new int[3];
-            Arrays.fill(min, Integer.MAX_VALUE);
-            bfsExplore();
-            if (min[2] == Integer.MAX_VALUE) break;
-            totalTime += min[2];
-            queue.add(new int[] {min[0], min[1]});
-            map[min[0]][min[1]] = 0;
-    
+	private static boolean bfs() {
+		Queue<Shark> q = new LinkedList<>();
+		q.offer(new Shark(start[0], start[1], 0));
+		visited = new boolean[N][N];
+		visited[start[0]][start[1]] = true;
+		boolean check = false;
+		
+		a:
+		while(!q.isEmpty()) {			
+			Shark cur = q.poll();
+			int r = cur.r;
+			int c = cur.c;
+			int time = cur.time;
 
-            eat++;
-            if (eat == shark) {
-                shark++;
-                eat = 0;
-            }
+			for (int i = 0; i < 4; i++) {
+				int nr = r+dr[i];
+				int nc = c+dc[i];
+				
+				if(!check(nr, nc)) continue;
+				if(visited[nr][nc]) continue;
+				if(map[nr][nc] > shark) continue;
+				if(map[nr][nc] == shark || map[nr][nc] == 0) {
+					visited[nr][nc] = true;
+					q.offer(new Shark(nr, nc, time+1));
+					continue;
+				}
+				
+				if(map[nr][nc] > 0) {
+					pq.offer(new Shark(nr, nc, time+1));
+					visited[nr][nc] = true;
+					map[start[0]][start[1]] = 0;
+					check = true;
+					
+					continue a;
+				}
+			}
+		}
+		// 더이상 먹을 수 있는 물고기가 없으면
+		if(!check) return false;
+		return true;
+	}
 
-
-        }
-
-        System.out.println(totalTime);
-
-
-
-
-        br.close();
-    }
-
-    static void bfsExplore() {
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nr = cur[0] + dr[i];
-                int nc = cur[1] + dc[i];
-                // System.out.printf("%d %d\n", nr, nc );
-                if (!mapCheck(nr, nc) || isVisited[nr][nc] != 0 || map[nr][nc] > shark) continue;
-
-                if (map[nr][nc] == 0 || map[nr][nc] == shark) {
-                    queue.add(new int[]{nr, nc});
-                    isVisited[nr][nc] = isVisited[cur[0]][cur[1]] + 1;
-                } else if (map[nr][nc] < shark) {
-                    
-
-                    isVisited[nr][nc] = isVisited[cur[0]][cur[1]] + 1;
-                    if (min[2] < isVisited[nr][nc]) {
-                        break;
-                    }
-                    min[2] = Math.min(min[2], isVisited[nr][nc]);
-
-
-                    if (min[0] == nr) {
-                        if (min[1] > nc) {
-                            min[0] = nr;
-                            min[1] = nc;
-                        }
-                    } else if (min[0] > nr) {
-                        min[0] = nr;
-                        min[1] = nc;
-                    }
-                    
-                }
-              
-            }
-            
-
-        }
-    }
-
-
-    // static void bfsExplore() {
-    //     while (!queue.isEmpty()) {
-    //         int[] cur = queue.poll();
-    //         // if (isVisited[cur[0]][cur[1]] == true) continue;
-    //         for (int i = 0; i < 4; i++) {
-    //             int nr = cur[0] + dr[i];
-    //             int nc = cur[1] + dc[i];
-    //             if (!mapCheck(nr, nc) || isVisited[nr][nc] != 0 || map[nr][nc] > shark) continue;
-
-    //             // 
-    //             if (map[nr][nc] == 0 || map[nr][nc] == shark) {
-    //                 queue.add(new int[]{nr, nc});
-    //                 isVisited[nr][nc] = isVisited[cur[0]][cur[1]] + 1;
-    //             } else if (map[nr][nc] < shark) {
-
-            
-
-    //                 queue.clear();
-    //                 queue.add(new int[]{nr, nc});
-    //                 isVisited[nr][nc] = isVisited[cur[0]][cur[1]] + 1;
-    //                 map[nr][nc] = 0;
-    //                 eat++;
-    //                 if (eat == shark) {
-    //                     shark++;
-    //                     eat = 0;
-    //                 }
-    //                 totalTime += isVisited[nr][nc];
-    //                 System.out.println(shark);
-    //                 System.out.printf("%d %d \n", nr + 1, nc + 1);
-    //                 System.out.println(isVisited[nr][nc]);
-    //                 System.out.println(totalTime);
-    //                 System.out.println();
-    //                 isVisited = new int[N][N];
-    //                 break;
-    //             }
-              
-    //         }
-            
-
-    //     }
-    // }
-
-    static boolean mapCheck(int r, int c) {
-        return r >= 0 && r < N && c >= 0 && c < N;
-    }
-
-    static void mapInit() {
-        for (int i = 0; i < N; i++) {
-            // for (int j = 0; j < N)
-        }
-    }
+	private static boolean check(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < N;
+	}
 }
-
-
