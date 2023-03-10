0a46ca5320d024b7efcbccfa6b01b30230a8d5af
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-03-10 22:39:11 +0900

    [Silver III] Title: 등수 매기기, Time: 620 ms, Memory: 52724 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 등수 매기기
+
+public class Main {
+    static StringBuilder sb;
+    static int N;
+    static int[] numbers;
+    static boolean[] isSelected;
+    static long cnt;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
+    }
+    
+    private static void print() {
+        System.out.println(cnt);
+    }
+
+    private static void solve() {
+        Arrays.sort(numbers);
+        for (int i = 1; i < numbers.length; i++) {
+            cnt += Math.abs(i - numbers[i]);
+        }
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        numbers = new int[N + 1];
+        isSelected = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
