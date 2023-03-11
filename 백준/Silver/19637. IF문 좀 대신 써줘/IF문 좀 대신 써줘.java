8cbec9920c3ad6cf3e06ce0e3251361bf3d5f6b9
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-03-11 11:46:20 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 436 ms, Memory: 62268 KB -BaekjoonHub

---
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+// IF문 좀 대신 써줘
+// 이분 탐색
+
+public class Main {
+    static StringBuilder sb;
+    static int N, M;
+    static Style[] list;
+
+    static class Style {
+        String name;
+        int number;
+        public Style(String name, int number) {
+            this.name = name;
+            this.number = number;
+        }
+        
+        
+    }
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+
+    private static void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        list = new Style[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int number = Integer.parseInt(st.nextToken());
+            list[i] = new Style(name, number);
+        }
+
+        for (int i = 0; i < M; i++) {
+            int current = Integer.parseInt(br.readLine());
+            int low = -1;
+            int high = N;
+            while (low + 1 < high) {
+                int mid = (low + high) / 2;
+                if (list[mid].number < current) {
+                    low = mid;
+                } else {
+                    high = mid;
+                }
+            }
+            sb.append(list[high].name).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+
+
+
+
+    }
+}
