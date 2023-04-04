add5bcb2ab8d87701e4209d9d9d683559da01dcd
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-04-04 15:44:05 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,345 ms, Memory: 95,432 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 키순서
+
+public class Solution {
+    static int T, N, M;
+    static int[][] dp;
+    static final int INF = 501;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            N = Integer.parseInt(br.readLine());
+            M = Integer.parseInt(br.readLine());
+            dp = new int[N + 1][N + 1];
+            StringTokenizer st;
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                dp[s][e] = 1;
+            }
+
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (i == j || dp[i][j] == 0) {
+                        dp[i][j] = 501;
+                    }
+                }
+            }
+
+
+            for (int k = 1; k <= N; k++) {
+                for (int i = 1; i <= N; i++) {
+                    for (int j = 1; j <= N; j++) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+                    }
+                }
+            }
+            int person = 0;
+            for (int i = 1; i <= N; i++) {
+                int cnt = 0;
+                for (int j = 1; j <= N; j++) {
+                    if (i != j) {
+                        if (dp[i][j] != INF) {
+                            cnt++;
+                        } else if (dp[i][j] == INF && dp[j][i] != INF) {
+                            cnt++;
+                        }
+                    }
+                }
+
+                if (cnt == N - 1) person++;
+            }
+
+
+            sb.append("#").append(t).append(" ").append(person).append("\n");
+        }
+        System.out.println(sb.toString());
+        
+        
+    }
+}
