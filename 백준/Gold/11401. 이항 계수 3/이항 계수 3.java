8bc07675a6dc79f40ffeeb9f5b83bddb91136298
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-04-06 16:55:28 +0900

    [Gold I] Title: 이항 계수 3, Time: 196 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 이항계수 3
+
+public class Main {
+    static int N, R;
+    static int P = 1000000007;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        solve();
+    }
+    private static void solve() throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        long result = nCr(N, R);
+        sb.append(result);
+
+        System.out.println(sb.toString());
+        
+    }
+    private static long nCr(long n, long r) {
+        long result = 1l;
+        result *= fact(n);
+        result = (result * power(fact(n - r), P - 2)) % P;
+        result = (result * power(fact(r), P - 2)) % P;
+        return result;
+    }
+    private static long fact(long n) {
+        long result = 1l;
+        for (int i = 1; i <= n; i++) {
+            result = (result * i) % P;
+        }
+        return result;
+    }
+
+    static long power(long x, long y) {
+        long result = 1l;
+        while (y > 0) {
+            if (y % 2 == 1) {
+                result = (result * x) % P;
+            }
+
+            x = (x * x) % P;
+            y /= 2;
+        }
+
+        return result;
+    }
+}
