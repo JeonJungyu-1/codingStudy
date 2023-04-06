090153fac22b8b65603d831d3153c5250a5493d5
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-04-06 23:56:22 +0900

    [Gold I] Title: 이항 계수 3, Time: 160 ms, Memory: 42400 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
@@ -7,8 +7,9 @@ import java.util.StringTokenizer;
 // 이항계수 3
 
 public class Main {
-    static int N, R;
-    static int P = 1000000007;
+    static long N, R;
+    static long P;
+    static long[] memo;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         solve();
@@ -18,38 +19,58 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
-        long result = nCr(N, R);
+        N = Long.parseLong(st.nextToken());
+        R = Long.parseLong(st.nextToken());
+        P = 1000000007;
+        memo = new long[(int)N + 1];
+        memoFact(P);
+        long result = nCr(N, R, P);
         sb.append(result);
 
         System.out.println(sb.toString());
         
     }
-    private static long nCr(long n, long r) {
-        long result = 1l;
-        result *= fact(n);
-        result = (result * power(fact(n - r), P - 2)) % P;
-        result = (result * power(fact(r), P - 2)) % P;
-        return result;
-    }
-    private static long fact(long n) {
-        long result = 1l;
-        for (int i = 1; i <= n; i++) {
-            result = (result * i) % P;
+
+    private static void memoFact(long p) {
+        memo[0] = 1;
+        for (int i = 1; i < memo.length; i++) {
+            memo[i] = (memo[i - 1] * i) % p;
         }
+    }
+
+    private static long nCr(long n, long r, long p) {
+        long result = 1l;
+
+        if (n < p) {
+            return ((memo[(int)n] * modInverse(memo[(int)r], p)) % p) * modInverse(memo[(int)(n - r)], p) % p;
+        } else {
+            while (n > 0 || r > 0) {
+                long a = n % p;
+                long b = r % p;
+                if (a < b) return 0;
+                result = (result * memo[(int)a]) % p;
+                result = (result * modInverse((memo[(int)b] * memo[(int)a - (int)b]) % p, p)) % p;
+                n /= p;
+                r /= p;
+            }
+        }
+
         return result;
     }
 
-    static long power(long x, long y) {
+    static long modInverse(long n, long p) { 
+        return power(n, p-2, p); 
+    } 
+
+    static long power(long x, long y, long p) {
         long result = 1l;
         while (y > 0) {
-            if (y % 2 == 1) {
-                result = (result * x) % P;
+            if ((y & 1) != 0) {
+                result = (result * x) % p;
             }
 
             x = (x * x) % P;
-            y /= 2;
+            y >>= 1;
         }
 
         return result;
