95ef2cf59e65b6ea32a9fc28f971fd950eb003a9
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-04-14 09:03:12 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 596 ms, Memory: 54016 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+// 좌표 정렬하기
+
+public class Main {
+    static int N;
+    static class Node implements Comparable<Node> {
+        int r, c;
+
+        public Node(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            int compare = Integer.compare(this.r, o.r);
+            return compare == 0 ? Integer.compare(this.c, o.c) : compare;
+        }
+
+        
+        
+    }
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            pq.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+            sb.append(cur.r).append(" ").append(cur.c).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+    }
+}
