9c6ab31539cc27daf6432bfad58d9cc116128bff
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-05-17 21:20:23 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 20784 ms, Memory: 214544 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -16,20 +19,40 @@ public class Main {
     }
     
 
+    // 우선순위큐
+    // private static void solve() throws IOException {
+    //     System.setIn(new FileInputStream("baekjoon/a.txt"));
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     StringTokenizer st;
+    //     sb = new StringBuilder();
+    //     int N = Integer.parseInt(br.readLine());
+    //     Queue<Integer> pq = new PriorityQueue<>();
 
+    //     for (int i = 0; i < N; i++) {
+    //         pq.offer(Integer.parseInt(br.readLine()));
+    //     }
+
+    //     while (!pq.isEmpty()) {
+    //         sb.append(pq.poll()).append("\n");
+    //     }
+
+    //     System.out.println(sb.toString());
+    // }
+
+    // 동적배열
     private static void solve() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        Queue<Integer> pq = new PriorityQueue<>();
+        List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            pq.offer(Integer.parseInt(br.readLine()));
+            list.add(Integer.parseInt(br.readLine()));
         }
-
-        while (!pq.isEmpty()) {
-            sb.append(pq.poll()).append("\n");
+        Collections.sort(list);
+        for (int i = 0; i < N; i++) {
+            sb.append(list.get(i)).append("\n");
         }
 
         System.out.println(sb.toString());
