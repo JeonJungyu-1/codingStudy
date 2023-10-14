4e972d3aee87f5f7e1de0e1c32eb93b0e73cf26e
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-10-14 15:17:44 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,15 @@
+# 시험 성적
+
+N = int(input())
+
+
+if 90 <= N <= 100:
+    print("A")
+elif N >= 80:
+    print("B")
+elif N >= 70:
+    print("C")
+elif N >= 60:
+    print("D")
+else:
+    print("F")
