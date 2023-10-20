aa6a1bd592e0f6fba4019397d0ae779d0e029d21
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-10-20 23:14:24 +0900

    [level 2] Title: 소수 찾기, Time: 4.13 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    static Set<Integer> set;
+    static String[] numberArr;
+    
+    public int solution(String numbers) {
+        int answer = 0;
+        set = new HashSet<>();
+        
+        numberArr = numbers.split("");
+       
+        
+        
+        // nP1 ... nPn
+        for (int i = 1; i <= numbers.length(); i++) {
+            boolean[] visited = new boolean[numbers.length()];
+            perm(0, "", i, visited);
+        }        
+        
+        for (int number : set) {
+            if (isPrime(number)) answer++;
+        }
+        
+        return answer;
+    }
+    
+    
+    public boolean isPrime(int number) {
+        if (number == 1 || number == 0) return false;
+        
+        double sqrt = Math.sqrt(number);
+        
+        for (int i = 2; i <= sqrt; i++) {
+            if (number % i == 0) return false;
+        }
+        
+        return true;
+    }
+    
+    public void perm(int cnt, String num, int n, boolean[] visited) {
+        if (cnt == n) {
+            set.add(Integer.parseInt(num));
+            return;
+        }
+        
+        
+        for (int i = 0; i < numberArr.length; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            perm(cnt + 1, num + numberArr[i], n, visited);
+            visited[i] = false;
+        }
+    }
+}
