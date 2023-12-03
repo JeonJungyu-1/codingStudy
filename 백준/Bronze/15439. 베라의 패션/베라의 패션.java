22ad333890be169b2941dfe9dec3d4ccb308832f
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-12-03 15:51:23 +0900

    [Bronze IV] Title: 베라의 패션, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 베라의 패션
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N * N - N);
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
