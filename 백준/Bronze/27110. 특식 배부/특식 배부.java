872e0a6def9524902f95a09bec4174ac2b2523ef
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2023-12-28 15:10:50 +0900

    [Bronze IV] Title: 특식 배부, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 특식 배부
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int result = Math.min(N, A) + Math.min(N, B) + Math.min(N, C);
+        System.out.println(result);
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
