18b33b9a5354dfcf13596cb91a21b80fe1eb74f2
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-01-11 19:36:06 +0900

    [Bronze V] Title: 나는 누구인가, Time: 72 ms, Memory: 11316 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 나는 누구인가
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        System.out.println("I'm Sexy");
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
