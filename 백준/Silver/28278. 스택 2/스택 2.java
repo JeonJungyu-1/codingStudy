1990f51ed0900149c0a6a5d4d200f305544ef395
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-01-30 10:44:51 +0900

    [Silver IV] Title: 스택 2, Time: 988 ms, Memory: 246136 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+// 스택 2
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+
+        int T = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            switch (command) {
+                case 1:
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case 2:
+                    sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+                    break;
+                case 3:
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case 4:
+                    sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case 5:
+                    sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+                    break;
+           
+            }
+
+        }
+
+        System.out.println(sb.toString());
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
