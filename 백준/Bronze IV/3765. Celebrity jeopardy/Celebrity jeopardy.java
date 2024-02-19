fcc02459dbe3b8b1e7d0d0edb6ae3fe81c020cde
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-02-19 20:19:03 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 76 ms, Memory: 11312 KB -BaekjoonHub

---
+++ 백준/Bronze IV/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Celebrity jeopardy
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+        while (true) {
+            String str = br.readLine();
+            if (str == null) break;
+            sb.append(str).append("\n");
+        }
+        System.out.println(sb.toString());
+
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
