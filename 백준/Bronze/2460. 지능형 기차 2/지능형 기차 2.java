55361a8021b08351b0352c682b3dca19dd5e294a
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-02-21 18:52:21 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 76 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 지능형 기차 2
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        int max = Integer.MIN_VALUE;
+        int cur = 0;
+        for (int i = 0; i < 10; i++) {
+            st = new StringTokenizer(br.readLine());
+            int out = Integer.parseInt(st.nextToken());
+            int in = Integer.parseInt(st.nextToken());
+            
+            cur -= out;
+            if (cur < 0) cur = 0;
+            
+            cur += in;
+            if (cur >= 10000) cur = 10000;
+            
+            max = Math.max(max, cur);
+        }
+
+        System.out.println(max);
+
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
