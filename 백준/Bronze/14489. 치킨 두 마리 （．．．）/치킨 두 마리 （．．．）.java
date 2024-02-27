699abe6e6960339ecb17f575679ef80bb6cde66f
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-02-27 18:53:31 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 80 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 치킨 두 마리
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int chicken = Integer.parseInt(br.readLine());
+
+        if ((a + b) >= (chicken * 2)) {
+            System.out.println(a + b - (chicken * 2));
+        } else {
+            System.out.println(a + b);
+        }   
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
