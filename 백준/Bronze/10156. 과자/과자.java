3be0f7034f0f289accf2a73ce1b12703b716cd1d
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-03-04 19:09:28 +0900

    [Bronze IV] Title: 과자, Time: 80 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 과자
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int result = (N * K) - M;
+        if (result < 0) result = 0;
+        System.out.println(result);
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
