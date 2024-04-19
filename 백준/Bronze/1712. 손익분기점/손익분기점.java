95230c1ff2adc3e8e245c1421c49eeee675776e5
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-04-19 18:49:10 +0900

    [Bronze II] Title: 손익분기점, Time: 76 ms, Memory: 11440 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 손익분기점
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if (B >= C) {
+            System.out.println(-1);
+            return;
+        }
+
+        int diff = C - B;
+        System.out.println(A/diff + 1);
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
