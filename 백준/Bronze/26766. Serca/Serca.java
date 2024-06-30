3f67ace4e7ebe246e95ec4d39a1ecb9f5ac564c1
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-06-30 12:31:18 +0900

    [Bronze V] Title: Serca, Time: 240 ms, Memory: 90364 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Serca
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            sb.append(" @@@   @@@ \n" + //
+            "@   @ @   @\n" + //
+            "@    @    @\n" + //
+            "@         @\n" + //
+            " @       @ \n" + //
+            "  @     @  \n" + //
+            "   @   @   \n" + //
+            "    @ @    \n" + //
+            "     @     \n");
+        }
+
+        System.out.println(sb.toString());
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
