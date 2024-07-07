3bd143e0e3c676ff191204c1b856bc71a21b3090
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-07-07 12:38:27 +0900

    [Bronze V] Title: Киноманы, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/28927. Киноманы/Киноманы.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Киноманы
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+
+        int[] score = new int[2];
+        for (int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            score[i] = Integer.parseInt(st.nextToken()) * 3 + Integer.parseInt(st.nextToken()) * 20 + Integer.parseInt(st.nextToken()) * 120;
+        }
+
+        if (score[0] > score[1]) {
+            System.out.println("Max");
+        } else if (score[0] < score[1]) {
+            System.out.println("Mel");
+        } else {
+            System.out.println("Draw");
+        }
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
