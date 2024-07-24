74b720656699840f424869a744372f1306fa1e27
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-07-24 23:10:09 +0900

    [Bronze V] Title: Bottle Return, Time: 80 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Bottle Return
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        for (int i = 0; i < 6; i++) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(sum * 5);
+
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
