a9fd1c9e544a4f8874aceed3edd4ff568776ce9e
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-08-01 19:21:57 +0900

    [Bronze V] Title: Multiply, Time: 588 ms, Memory: 53976 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+// Multiply
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+
+
+        br.readLine();
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+
+        System.out.println(a.multiply(b).toString());
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
