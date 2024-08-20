2e15f84c030dd3bee9575213e0f5db50ce4a2370
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-08-21 02:33:09 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 64 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 뉴비의 기준은 뭘까?
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if (M == 1 || M == 2) {
+            System.out.println("NEWBIE!");
+        } else if (M <= N) {
+            System.out.println("OLDBIE!");
+        } else {
+            System.out.println("TLE!");
+        }
+
+
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
