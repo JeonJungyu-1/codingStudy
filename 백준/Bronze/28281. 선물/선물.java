5b3a45ae369585b30057a36e8216641f0ce66f95
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-09-01 11:51:13 +0900

    [Bronze IV] Title: 선물, Time: 212 ms, Memory: 26368 KB -BaekjoonHub

---
+++ 백준/Bronze/28281. 선물/선물.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 선물
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int[] numbers = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        Queue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N - 1; i++) {
+            pq.offer(numbers[i] + numbers[i + 1]);
+        }
+
+        System.out.println(pq.peek() * X);
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
