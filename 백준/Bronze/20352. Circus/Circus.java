766a5375f3e2680b4104a57852a6c6741d1c8653
전준규 <85854928+JeonJungyu-1@users.noreply.github.com>
2024-09-27 18:49:04 +0900

    [Bronze IV] Title: Circus, Time: 68 ms, Memory: 11664 KB -BaekjoonHub

---
+++ 백준/Bronze/20352. Circus/Circus.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// Circus
+
+public class Main {
+    
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+    
+    private static void print() {
+
+    }
+
+    private static void solve() throws IOException {
+        init();
+        print();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        sb = new StringBuilder();
+
+        double a = Double.parseDouble(br.readLine());
+
+        double r = Math.sqrt(a/Math.PI);
+        System.out.println(2 * Math.PI * r);
+
+
+
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();    
+    }
+}
